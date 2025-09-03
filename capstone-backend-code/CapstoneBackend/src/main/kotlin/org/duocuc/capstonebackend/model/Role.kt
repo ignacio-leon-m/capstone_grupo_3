0e4ed0316931d44b94ed698cdebbfef5b86cd892
ignacio-leon-m <ig.leon@duocuc.cl>
2025-09-03 15:30:13 -0400

    Usuario: hasheo password, lógica de obtención de usuario por mail para el login

--- capstone-backend-code/CapstoneBackend/src/main/kotlin/org/duocuc/capstonebackend/model/Role.kt
+++ capstone-backend-code/CapstoneBackend/src/main/kotlin/org/duocuc/capstonebackend/model/Role.kt
@@ -2,10 +2,19 @@ package org.duocuc.capstonebackend.model
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+
+
+enum class RoleType {
+    ADMIN,
+    STUDENT,
+    TEACHER
+}
 
 @Entity
 @Table(name = "roles")
@@ -13,6 +22,8 @@ data class Role (
     @Id
     @GeneratedValue(strategy = GenerationType.UUID) // UUID
     val id: Long = 0,
+
+    @Enumerated(EnumType.STRING)
     @Column(name = "nombre_rol", nullable = false, length = 50)
-    val roleName: String = ""
+    val roleName: RoleType
 )
--- capstone-backend-code/CapstoneBackend/src/main/kotlin/org/duocuc/capstonebackend/repository/UserRepository.kt
+++ capstone-backend-code/CapstoneBackend/src/main/kotlin/org/duocuc/capstonebackend/repository/UserRepository.kt
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository : JpaRepository<User, Long> {
     fun findByName(name: String): User?
+    fun findByEmail(email: String): User?
 }
