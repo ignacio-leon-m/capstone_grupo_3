328114c302acbc5dc60166630c049efb870bf695
ignacio-leon-m <ig.leon@duocuc.cl>
2025-10-01 14:47:56 -0300

    se agrega dependencia de springmockk:4.0.2

--- capstone-backend-code/CapstoneBackend/build.gradle.kts
+++ capstone-backend-code/CapstoneBackend/build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
