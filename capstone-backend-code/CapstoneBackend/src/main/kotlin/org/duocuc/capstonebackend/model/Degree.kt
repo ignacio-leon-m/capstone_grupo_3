f20b7521305eaef725bde8feb1dd22089193145d
ignacio-leon-m <ig.leon@duocuc.cl>
2025-10-05 01:52:50 -0300

    se agregan modelos score, degree y subject

--- capstone-backend-code/CapstoneBackend/src/main/kotlin/org/duocuc/capstonebackend/model/Degree.kt
+++ capstone-backend-code/CapstoneBackend/src/main/kotlin/org/duocuc/capstonebackend/model/Degree.kt
@@ -2,19 +2,17 @@ package org.duocuc.capstonebackend.model
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import java.util.UUID
 
 @Entity
-@Table(name = "roles")
-class Role (
+@Table(name = "carreras")
+class Degree(
     @Id
     @Column(columnDefinition = "uuid", updatable = false, nullable = false)
     val id: UUID = UUID.randomUUID(),
 
-    @Column(name = "nombre_rol", nullable = false, length = 100)
+    @Column(name = "nombre", nullable = false, length = 100)
     val name: String = ""
 )
--- capstone-backend-code/CapstoneBackend/src/main/kotlin/org/duocuc/capstonebackend/model/Subject.kt
+++ capstone-backend-code/CapstoneBackend/src/main/kotlin/org/duocuc/capstonebackend/model/Subject.kt
@@ -2,19 +2,23 @@ package org.duocuc.capstonebackend.model
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import java.util.UUID
 
 @Entity
-@Table(name = "roles")
-class Role (
+@Table(name = "materias")
+class Subject (
     @Id
     @Column(columnDefinition = "uuid", updatable = false, nullable = false)
     val id: UUID = UUID.randomUUID(),
 
-    @Column(name = "nombre_rol", nullable = false, length = 100)
-    val name: String = ""
+    @Column(name = "nombre", nullable = false, length = 100)
+    val name: String = "",
+
+    @ManyToOne
+    @JoinColumn(name = "id_carrera", nullable = false)
+    val degree: Degree
 )
